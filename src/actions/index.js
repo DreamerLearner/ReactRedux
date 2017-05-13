1ae1b9bc31cdf8b46569df5e3ea0859a2952a548
DreamerLearner <sneha.26.parab@gmail.com>
2017-05-13 17:11:54 +0530

    removed wrong code

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_KEY = '1e8f8c6ed0a585a9cdac1ca432bcf002';
+const API_KEY = '1e662ba92a8208801bbb28e7cb08b40a';
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`; 
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
