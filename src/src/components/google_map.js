2d3cf9a676b9aa03bb5c2a92593e48d2ecadda7a
DreamerLearner <sneha.26.parab@gmail.com>
2017-05-13 16:45:00 +0530

    Add files via upload

---
+++ src/src/components/google_map.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class GoogleMap extends Component {
+
+	componentDidMount() {
+		new google.maps.Map( this.refs.map , {
+			zoom : 12,
+			center : {
+				lat : parseInt(this.props.lat),
+				lng : parseInt(this.props.lon)
+			}
+		});
+	}
+
+	render() {
+		return <div ref="map" />;
+	}
+
+}
+
+export default GoogleMap;
